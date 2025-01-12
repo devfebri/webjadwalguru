f43a3fc8226bb0dd526cde51b39e12e29acd390a
Febri <dev.pebri@gmail.com>
2025-01-12 20:03:43 +0700

    bug delete

--- app/Http/Controllers/Admin/GuruController.php
+++ app/Http/Controllers/Admin/GuruController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Guru;
+use App\Models\Schedule;
 use Str;
 use Storage;
 
--- app/Http/Controllers/Admin/SubjectController.php
+++ app/Http/Controllers/Admin/SubjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Models\Subject;
+use App\Models\Schedule;
 use App\Models\Guru;
 use App\User;
 use Illuminate\Support\Facades\Auth;
